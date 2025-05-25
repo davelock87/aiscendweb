2799676b2e5162b76c9ca5cb99805974f63e96ef
davelock87 <aiscendcol@gmail.com>
2025-05-25 11:32:40 -0500

    Create vCard page with QR code

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import RestaurantAutomation from './components/RestaurantAutomation';
 import Academy from './components/Academy';
+import VCard from './components/VCard';
 
 function App() {
   const { language } = useLanguage();
@@ -36,6 +37,8 @@ function App() {
         return <RestaurantAutomation />;
       case '/academy':
         return <Academy />;
+      case '/about':
+        return <VCard />;
       default:
         return (
           <>
