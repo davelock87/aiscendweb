d9858482aa595da88fd4c99b559e9d33ddd860dd
davelock87 <aiscendcol@gmail.com>
2025-05-25 11:33:17 -0500

    Add default export to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,22 @@
-{/* Update the About Us link in the Footer component */}
-<a href="/about" className="text-white/60 hover:text-primary transition-colors">
-  {t('footer.about')}
-</a>
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-between items-center">
+          <div className="flex space-x-4">
+            <a href="/about" className="text-white/60 hover:text-primary transition-colors">
+              {t('footer.about')}
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
